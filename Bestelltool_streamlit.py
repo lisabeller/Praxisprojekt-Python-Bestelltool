c2e9103bcfa02686489ed65d621c8c8f889168fb
lisa <lisamariebeller@gmail.com>
2024-06-05 15:36:42 +0200

    Rechnung und stornierung hinzugefügt

--- Bestelltool_streamlit.py
+++ Bestelltool_streamlit.py
@@ -18,7 +18,7 @@ bestellungen_df = pd.DataFrame(columns=[
                                 "Status"]
                                 )
 
-# Funktion zur Erstellung einer neuen Bestellung
+# FUNKTION NEUE BESTELLUNG
 def neue_bestellung(tischnummer, speise_mengen, bestellungen_df, status="offen"):
     neue_bestellungen = []
     
@@ -41,8 +41,74 @@ def neue_bestellung(tischnummer, speise_mengen, bestellungen_df, status="offen")
     
     return bestellungen_df
 
-# STREAMLIT APP
+# FUNKTION BESTELLUNG STORNIEREN
+def bestellung_storno(bestellID, bestellungen_df = bestellungen_df):
+    bestellungen_df.loc[bestellungen_df["BestellID"] == bestellID, "Status"] = "storno"  
 
+    return bestellungen_df
+
+# FUNKTION RECHNUNG ERSTELLEN
+def brutto_nettorechner(bruttopreis, stueckzahl, mwst=0.19):
+    gesamt_bruttopreis = bruttopreis * stueckzahl
+    nettopreis = gesamt_bruttopreis / (1 + mwst)
+    steuerbetrag = gesamt_bruttopreis - nettopreis
+
+    return gesamt_bruttopreis, nettopreis, steuerbetrag
+
+def bestellung_bezahlen(bestellID, bestellungen_df = bestellungen_df, speisekarte=speisekarte):
+    # Status auf "bezahlt" setzten
+    bestellungen_df.loc[bestellungen_df["BestellID"] == bestellID, "Status"] = "bezahlt"
+
+    # Filtert die Bestellungen nach der BestellID
+    bestellung_details = bestellungen_df[bestellungen_df["BestellID"] == bestellID]
+    
+    # Funktion für DataFrame der Rechnung
+    def berechne_rechnung(row):
+        
+        # Variablen
+        speise_id = row["SpeiseID"]
+        menge = row["Menge"]
+        bruttopreis = speisekarte.loc[speise_id, "Preis"]
+
+        # Datum ohne Uhrzeit formatieren
+        datum = datetime.strptime(row["Datum"], "%Y-%m-%d %H:%M:%S").strftime("%Y-%m-%d")
+
+        # Errechnet Netto und Steuerbetrag
+        gesamt_brutto, netto, steuer = brutto_nettorechner(bruttopreis, menge)
+
+        return pd.Series({
+            "BestellID": row["BestellID"],
+            "Datum": datum,
+            "Tischnr": row["Tischnummer"],
+            "SpeiseID": speise_id,
+            "Menge": menge,
+            "Nettopreis": round(netto, 2),
+            "Steuer(19%)": round(steuer, 2),
+            "Bruttopreis": round(gesamt_brutto, 2),
+        })
+    # Berechnet die Rechnung für jede Zeile in bestellung_details
+    rechnung_df = bestellung_details.apply(berechne_rechnung, axis=1)
+
+    # Gesamtbetrag in neue Zeile einfügen
+    letzte_zeile = rechnung_df.iloc[-1]
+
+    neue_zeile_df = pd.DataFrame({
+                    "BestellID": ["Gesamt"],
+                    "Datum": [""],
+                    "Tischnr": [""],
+                    "SpeiseID": [""],
+                    "Menge": [rechnung_df["Menge"].sum()],
+                    "Nettopreis": [rechnung_df["Nettopreis"].sum()],
+                    "Steuer(19%)": [rechnung_df["Steuer(19%)"].sum()],
+                    "Bruttopreis": [rechnung_df["Bruttopreis"].sum()]
+                    })
+
+    rechnung_df = pd.concat([rechnung_df, neue_zeile_df], ignore_index=True)
+    
+    return rechnung_df
+
+
+# STREAMLIT APP
 def streamlit_app():
     """
     Definiert den Ablauf der App, sammelt die Eingaben und aktualisiert das DataFrame.
@@ -114,12 +180,35 @@ def streamlit_app():
     
     st.subheader("Alle Bestellungen")
     st.dataframe(st.session_state.bestellungen_df)
+    
+    st.subheader("Bestellung stornieren")
+
+    vorhandene_bestellIDs = st.session_state.bestellungen_df["BestellID"].unique().tolist()
+    ausgewählte_bestellID = st.selectbox("BestellID", options=vorhandene_bestellIDs)
+
+    # Auswahl der BestellID über Dropdown
+    if st.button("Bestellung stornieren"):
+        if ausgewählte_bestellID:
+            st.session_state.bestellungen_df = bestellung_storno(ausgewählte_bestellID, 
+                                                st.session_state.bestellungen_df)
+            st.write(f"Bestellung {ausgewählte_bestellID} wurde storniert.")
+        else:
+            st.write("Bitte eine gültige BestellID auswählen.")
+
+    st.subheader("Rechnung erstellen")
+    # Auswahl der BestellID über Dropdown
+    if st.button("Rechnung anzeigen"):
+        if ausgewählte_bestellID:
+            rechnung_df = bestellung_bezahlen(ausgewählte_bestellID, st.session_state.bestellungen_df, speisekarte)
+            st.write("Rechnung für BestellID: ", ausgewählte_bestellID)
+            st.dataframe(rechnung_df)
+        else:
+            st.write("Bitte eine gültige BestellID auswählen.")
 
 if __name__ == "__main__":
     streamlit_app()
 
 
-# Aktuell werden in das DataFrame bestellungen_df noch nicht alle Bestellungen gesammelt
 
 # Ausführen des Skripts in Kommandozeile (Miniconda)
 # cd "C:\Users\Admin\Git\Praxisprojekt-Python-Bestelltool"
